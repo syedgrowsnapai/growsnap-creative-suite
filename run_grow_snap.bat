bdc60a83a07beb311d54a82b840ed3b5335eec0e
Syed Hussain <skhradiantexpressions@gmail.com>
2026-07-05 09:32:11 +0000

    Fix run_grow_snap.bat nested parenthesis syntax error

--- run_grow_snap.bat
+++ run_grow_snap.bat
@@ -7,19 +7,7 @@ echo.
 
 :: 1. If virtual env already exists, run the application directly
 if exist .venv\Scripts\python.exe (
-    if not exist "%userprofile%\Desktop\GrowSnap One.lnk" (
-        echo Creating Desktop Shortcut...
-        echo Set oWS = CreateObject("WScript.Shell") > "%temp%\CreateShortcut.vbs"
-        echo sLinkFile = "%userprofile%\Desktop\GrowSnap One.lnk" >> "%temp%\CreateShortcut.vbs"
-        echo Set oLink = oWS.CreateShortcut(sLinkFile) >> "%temp%\CreateShortcut.vbs"
-        echo oLink.TargetPath = "%~dp0run_grow_snap.bat" >> "%temp%\CreateShortcut.vbs"
-        echo oLink.WorkingDirectory = "%~dp0" >> "%temp%\CreateShortcut.vbs"
-        echo oLink.Description = "Launch GrowSnap One" >> "%temp%\CreateShortcut.vbs"
-        echo oLink.IconLocation = "%~dp0grow_snap_dola\dola_automation\resources\icon.ico" >> "%temp%\CreateShortcut.vbs"
-        echo oLink.Save >> "%temp%\CreateShortcut.vbs"
-        cscript /nologo "%temp%\CreateShortcut.vbs" >nul 2>&1
-        del "%temp%\CreateShortcut.vbs" >nul 2>&1
-    )
+    if not exist "%userprofile%\Desktop\GrowSnap One.lnk" call :create_shortcut
     echo Starting GrowSnap One...
     start "" ".venv\Scripts\python.exe" "grow_snap_dola/main.py" %*
     exit /b
@@ -72,19 +60,22 @@ python -m pip install --upgrade pip
 pip install pyqt6 patchright requests yt-dlp gTTS
 
 :: Create Desktop Shortcut on first run
-if not exist "%userprofile%\Desktop\GrowSnap One.lnk" (
-    echo Creating Desktop Shortcut...
-    echo Set oWS = CreateObject("WScript.Shell") > "%temp%\CreateShortcut.vbs"
-    echo sLinkFile = "%userprofile%\Desktop\GrowSnap One.lnk" >> "%temp%\CreateShortcut.vbs"
-    echo Set oLink = oWS.CreateShortcut(sLinkFile) >> "%temp%\CreateShortcut.vbs"
-    echo oLink.TargetPath = "%~dp0run_grow_snap.bat" >> "%temp%\CreateShortcut.vbs"
-    echo oLink.WorkingDirectory = "%~dp0" >> "%temp%\CreateShortcut.vbs"
-    echo oLink.Description = "Launch GrowSnap One" >> "%temp%\CreateShortcut.vbs"
-    echo oLink.IconLocation = "%~dp0grow_snap_dola\dola_automation\resources\icon.ico" >> "%temp%\CreateShortcut.vbs"
-    echo oLink.Save >> "%temp%\CreateShortcut.vbs"
-    cscript /nologo "%temp%\CreateShortcut.vbs" >nul 2>&1
-    del "%temp%\CreateShortcut.vbs" >nul 2>&1
-)
+if not exist "%userprofile%\Desktop\GrowSnap One.lnk" call :create_shortcut
 
 echo Starting GrowSnap One...
 python grow_snap_dola/main.py %*
+exit /b
+
+:create_shortcut
+echo Creating Desktop Shortcut...
+echo Set oWS = CreateObject("WScript.Shell") > "%temp%\CreateShortcut.vbs"
+echo sLinkFile = "%userprofile%\Desktop\GrowSnap One.lnk" >> "%temp%\CreateShortcut.vbs"
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> "%temp%\CreateShortcut.vbs"
+echo oLink.TargetPath = "%~dp0run_grow_snap.bat" >> "%temp%\CreateShortcut.vbs"
+echo oLink.WorkingDirectory = "%~dp0" >> "%temp%\CreateShortcut.vbs"
+echo oLink.Description = "Launch GrowSnap One" >> "%temp%\CreateShortcut.vbs"
+echo oLink.IconLocation = "%~dp0grow_snap_dola\dola_automation\resources\icon.ico" >> "%temp%\CreateShortcut.vbs"
+echo oLink.Save >> "%temp%\CreateShortcut.vbs"
+cscript /nologo "%temp%\CreateShortcut.vbs" >nul 2>&1
+del "%temp%\CreateShortcut.vbs" >nul 2>&1
+exit /b
